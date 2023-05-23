c2aad912fb340541b4cb2ac1bc95f70227a3d52e
Airdropcode <127380228+Airdropcode@users.noreply.github.com>
2023-05-23 10:49:52 +0700

    Update script.py

--- script.py
+++ script.py
@@ -1,40 +1,42 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import requests
+from bs4 import BeautifulSoup
 import time
 
 wallet_address = "alamat_dompet_anda"
 
 def claim(wallet):
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Menjalankan Chrome dalam mode headless (tanpa GUI)
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+    url = "https://pls-faucet.com/"
 
-    driver = webdriver.Chrome("/storage/emulated/0/pictures/chromedriver", options=chrome_options)  # Ganti dengan path sebenarnya ke ChromeDriver di direktori yang sesuai
-    driver.get("https://pls-faucet.com/")
+    # Buat session dan dapatkan halaman klaim
+    session = requests.Session()
+    response = session.get(url)
+
+    # Parsing halaman menggunakan BeautifulSoup
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Cari elemen input alamat dompet
+    wallet_input = soup.find("input", {"id": "wallet"})
+
+    if wallet_input is None:
+        print("Gagal mengklaim. Halaman tidak ditemukan.")
+        return
 
     # Isi alamat dompet
-    wallet_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "wallet")))
-    wallet_input.send_keys(wallet)
+    wallet_input["value"] = wallet
 
-    # Tunggu beberapa detik agar halaman selesai memuat
-    time.sleep(5)
+    # Kirim permintaan klaim
+    response = session.post(url, data=soup.form.attrs)
 
-    # Submit klaim
-    claim_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "claim")))
-    claim_button.click()
+    # Periksa hasil klaim
+    success_message = "Hadiah diklaim" in response.text
 
-    # Tunggu klaim selesai
-    success_message = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "success")))
-
-    driver.quit()
+    if success_message:
+        print("Berhasil mengklaim.")
+    else:
+        print("Gagal mengklaim.")
 
 if __name__ == '__main__':
     while True:
         claim(wallet_address)
-        print("Berhasil mengklaim.")
         print("Menunggu untuk klaim selanjutnya...")
         time.sleep(25 * 60)  # Tunggu selama 25 menit sebelum klaim berikutnya
